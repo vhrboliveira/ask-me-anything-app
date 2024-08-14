5b1504219b24895c12442b9ca34ad3a62c4bd102
Vitor Oliveira <vhrb.oliveira@gmail.com>
2024-08-14 18:08:37 +0100

    feat: add react query component

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,8 @@ import { Toaster } from "sonner";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { CreateRoom } from "./pages/create-room";
 import { Room } from "./pages/room";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 const router = createBrowserRouter([
   {
@@ -9,16 +11,16 @@ const router = createBrowserRouter([
     element: <CreateRoom />,
   },
   {
-    path: "/room/:room_id",
+    path: "/room/:roomId",
     element: <Room />,
   },
 ]);
 
 export function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <Toaster invert richColors />
-    </>
+    </QueryClientProvider>
   );
 }
