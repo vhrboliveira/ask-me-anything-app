b6a97b88629a0697af0c112bbd1c52aae6e90db2
Vitor Oliveira <vhrb.oliveira@gmail.com>
2024-08-14 18:10:46 +0100

    feat: update room/create room pages

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,27 +1,43 @@
 import { ArrowRight } from "lucide-react"
 import { useNavigate } from "react-router-dom"
 import logoImg from "../assets/icon.svg"
+import { createRoom } from "../http/create-room"
+import { toast } from "sonner"
 
 export function CreateRoom() {
   const navigate = useNavigate()
 
-  function handleCreateRoom(data: FormData) {
+  async function handleCreateRoom(data: FormData) {
     const name = data.get("name")?.toString()
 
-    navigate(`/room/${name}`)
+    if (!name) {
+      return
+    }
+
+    try {
+      const { roomId } = await createRoom({name})
+      
+      navigate(`/room/${roomId}`)
+    } catch (error) {
+      console.log("Error: ", error)
+      toast.error("Error while creating room")
+    }
+
   }
 
   return (
     <main className="h-screen flex items-center justify-center px-4">
       <div className="max-w-[450px] flex flex-col gap-6">
-        <img src={logoImg} alt="AMA" className="h-10" />
+        <a href="/#" className="h-10 m-auto">
+          <img src={logoImg} alt="AMA" className="h-10" />
+        </a>
         <p className="leading-relaxed text-zinc-300 text-center">
           Create a public AMA (Ask Me Anything) class, write the questions that you want and be happy!
         </p>
 
         <form 
           action={handleCreateRoom}
-          className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-zinc-950 ring-offset-2 focus-within:ring-1"
+          className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-green-400 ring-offset-zinc-950 ring-offset-2 focus-within:ring-1"
         >
           <input
             type="text"
@@ -29,9 +45,10 @@ export function CreateRoom() {
             placeholder="Room name" 
             autoComplete="off"
             className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500"
+            required
             />
 
-          <button type="submit" className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500">
+          <button type="submit" className="bg-green-400 text-green-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-green-500">
             Create Room
             <ArrowRight className="size-4" />
           </button>
--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,30 +1,34 @@
-import { ArrowRight, Share2 } from "lucide-react";
-import { useParams } from "react-router-dom";
-import { toast } from "sonner";
-import { Message } from "../components/message";
-import logoImg from "../assets/icon.svg";
+import { Share2 } from "lucide-react"
+import { Suspense } from "react"
+import { useParams } from "react-router-dom"
+import { toast } from "sonner"
+import logoImg from "../assets/icon.svg"
+import { CreateMessageForm } from "../components/create-message-form"
+import { Messages } from "../components/messages"
 
 export function Room() {
-  const { room_id } = useParams();
+  const { roomId } = useParams()
 
   function handleShareRoom() {
-    const url = window.location.href.toString();
+    const url = window.location.href.toString()
 
     if (navigator.share !== undefined && navigator.canShare()) {
-      navigator.share({ url });
+      navigator.share({ url })
     } else {
-      navigator.clipboard.writeText(url);
+      navigator.clipboard.writeText(url)
 
-      toast.info("The room URL was copied to your clipboard!");
+      toast.info("The room URL was copied to your clipboard!")
     }
   }
 
   return (
     <div className="mx-auto max-w-[640px] flex flex-col gap-6 py-10 px-4">
       <div className="flex items-center gap-3 px-3">
-        <img src={logoImg} className="size-5" />
+        <a href="/" className="p-0 m-0">
+          <img src={logoImg} className="size-5" />
+        </a>
         <span className="text-sm text-zinc-500 truncate">
-          Room Code: <span className="text-zinc-300">{room_id}</span>
+          Room Code: <span className="text-zinc-300">{roomId}</span>
         </span>
 
         <button
@@ -39,32 +43,11 @@ export function Room() {
 
       <div className="h-px w-full bg-zinc-900" />
 
-      <form className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-zinc-950 ring-offset-2 focus-within:ring-1">
-        <input
-          type="text"
-          name="message"
-          placeholder="What is your question?"
-          autoComplete="off"
-          className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500"
-        />
+      <CreateMessageForm />
 
-        <button
-          type="submit"
-          className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500"
-        >
-          Create Question
-          <ArrowRight className="size-4" />
-        </button>
-      </form>
-
-      <ol className="list-decimal list-outside px-3 space-y-8">
-        <Message text="Why are you learning Go?" reactionCount={12} />
-        <Message
-          text="What is concurrency (Solved)?"
-          reactionCount={10}
-          answered
-        />
-      </ol>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Messages />
+      </Suspense>
     </div>
-  );
+  )
 }
